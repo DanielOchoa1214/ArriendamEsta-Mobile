b499493ff88d96e94a5ac818f08f975c1bb007fa
DanielOchoa <danochoa1412@gmail.com>
2023-11-18 17:49:38 -0500

    Nuevo icono

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "edu.eci.arriendamestamobile"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "edu.eci.arriendamestamobile"
