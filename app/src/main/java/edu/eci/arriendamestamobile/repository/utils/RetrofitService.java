83b700cd11faa1ec92c5e7480452e8de188ed417
isaeme23 <isabella.manrique@mail.escuelaing.edu.co>
2023-11-29 15:46:42 -0500

    Change to localhost

--- app/src/main/java/edu/eci/arriendamestamobile/repository/utils/RetrofitService.java
+++ app/src/main/java/edu/eci/arriendamestamobile/repository/utils/RetrofitService.java
@@ -7,7 +7,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitService {
-    private static final String BASE_URL = "http://10.0.2.2:8080/";
+    private static final String BASE_URL = "http://localhost:8080/";
     private static final Retrofit retrofit = new Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
